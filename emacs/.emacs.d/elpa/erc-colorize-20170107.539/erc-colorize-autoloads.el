cf2cd9410aea1f17292e9f53aac6ecbd16b866d9
Dan <gocyclic249@gmail.com>
2018-02-02 10:41:51 -0700

    added non spacemacs emacs

---
+++ emacs/.emacs.d/elpa/erc-colorize-20170107.539/erc-colorize-autoloads.el
@@ -0,0 +1,18 @@
+;;; erc-colorize-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "erc-colorize" "erc-colorize.el" (23151 39374
+;;;;;;  2835 148000))
+;;; Generated autoloads from erc-colorize.el
+ (autoload 'erc-colorize-mode "erc-colorize")
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; erc-colorize-autoloads.el ends here
---
+++ emacs/.emacs.d/elpa/erc-colorize-20170107.539/erc-colorize-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "erc-colorize" "20170107.539" "Per user colorization of whole message" 'nil :commit "d026a016dcb9d63d9ac66d30627a92a8f1681bbd" :url "https://github.com/thisirs/erc-colorize.git" :keywords '("erc" "convenience"))
