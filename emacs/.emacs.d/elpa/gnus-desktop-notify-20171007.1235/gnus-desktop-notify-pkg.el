8b06bfecc632c2c7d682c96eaf4172fa25eecfb9
Dan <gocyclic249@gmail.com>
2018-02-03 21:03:02 -0700

    Tweakds to init.el

---
+++ emacs/.emacs.d/elpa/gnus-desktop-notify-20171007.1235/gnus-desktop-notify-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gnus-desktop-notify" "20171007.1235" "Gnus Desktop Notification global minor mode" '((gnus "1.0")) :commit "52e5de0961ce0f6890d5e7f26bc05152b99d9979" :url "http://www.thregr.org/~wavexx/software/gnus-desktop-notify.el/")
